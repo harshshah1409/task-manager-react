32044494681dd3b9f5f21ccadf426a474f411a9a
harshshah1409 <harsh140901@hotmail.com>
2026-03-10 15:39:56 +0530

    added task functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,16 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = () => {
+    if (task.trim() === "") return;
+
+    setTasks([...tasks, task]);
+    setTask("");
+  };
+
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h1>Task Manager</h1>
@@ -9,14 +19,19 @@ function App() {
       <input
         type="text"
         placeholder="Enter a task..."
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
         style={{ padding: "10px", width: "250px" }}
       />
 
-      <button style={{ marginLeft: "10px", padding: "10px" }}>Add Task</button>
+      <button onClick={addTask} style={{ marginLeft: "10px", padding: "10px" }}>
+        Add Task
+      </button>
 
       <ul style={{ marginTop: "30px" }}>
-        <li>Learn React</li>
-        <li>Practice Git</li>
+        {tasks.map((t, index) => (
+          <li key={index}>{t}</li>
+        ))}
       </ul>
     </div>
   );
